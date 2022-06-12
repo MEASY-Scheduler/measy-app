3bc89eade2abca7716eb520fb7c46ec87cbfc11e
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-06-12 01:37:43 +0100

    user validation & scaffold user request

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 
 class AuthController extends Controller
 {
 
-    public function register(Request $request)
+    public function register(UserRequest $request)
     {
-        
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->phone_no = $request->phone_no ? $request->phone_no : '';
+        $user->cell_phone_no = $request->cell_phone_no ? $request->cell_phone_no : '';
+        $user->organization = $request->organization ?  $request->organization : '';
+        $user->job_title = $request->job_title ? $request->job_title : '';
+        $user->occupation = $request->occupation ? $request->occupation : '';
     }
 
     public function show(User $user)
