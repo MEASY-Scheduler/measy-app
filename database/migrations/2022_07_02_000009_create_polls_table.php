f9df2b7131cc7b848254aa7ef1b2f6b7f81b35dd
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-07-02 01:50:25 +0100

    migrate polls schema and configured pgsql

--- database/migrations/2022_07_02_000009_create_polls_table.php
+++ database/migrations/2022_07_02_000009_create_polls_table.php
@@ -15,6 +15,25 @@ return new class extends Migration
     {
         Schema::create('polls', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+            $table->text('description');
+            $table->string('agenda');
+            $table->string('location');
+            $table->json('main_stakeholders');
+            $table->json('other_stakeholders');
+            $table->string('meeting_start_range');
+            $table->string('meeting_end_range');
+            $table->string('duration');
+            $table->string('no_of_entries');
+            $table->string('deadline_date_for_response');
+            $table->string('deadline_time_for_response');
+            $table->json('speakers');
+            $table->json('other_attendees');
+            $table->string('event_start_date_range');
+            $table->string('event_end_date_range');
+
+
             $table->timestamps();
         });
     }
